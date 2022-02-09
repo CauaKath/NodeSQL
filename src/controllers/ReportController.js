3c1ae24b035b201c993d20e1fb4fe38fcc3e5429
Cauã Kath <cauakathdev@gmail.com>
2022-02-09 11:40:04 -0300

    Node com SQL básico

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,39 @@
+const { Op } = require('sequelize');
+const User = require('../models/User');
+
+module.exports = {
+  async show(req, res) {
+    const users = await User.findAll({
+      attributes: ['name', 'email'],
+      where: {
+        email: {
+          [Op.like] : '%@gmail.com'
+        },
+      },
+      include: [
+        { 
+          association: 'addresses',
+          attributes: ['zipcode', 'street', 'number'],
+          where: {
+            street: 'Rua Alfredo Brunch' 
+          },
+        },
+        { 
+          association: 'techs',
+          required: false,
+          attributes: ['name'],
+          through: {
+            attributes: []
+          },
+          where: {
+            name: {
+              [Op.like]: 'Java%'
+            },
+          },
+        },
+      ],
+    });
+
+    return res.json(users);
+  }
+}
